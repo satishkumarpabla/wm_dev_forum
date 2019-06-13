a045d7765562b54bd1345dfc95ba83b1808c347f
Satish Kumar <satishkumar@TSTK.local>
2019-06-13 16:31:47 +0530

    user registration

---
+++ lib/services/user_management.ex
@@ -0,0 +1,5 @@
+defmodule WmDevForum.UserManagement do
+  def register_user(params) do
+    IO.inspect(params)
+  end
+end
--- lib/wm_dev_forum_web/controllers/page_controller.ex
+++ lib/wm_dev_forum_web/controllers/page_controller.ex
@@ -1,7 +1,22 @@
 defmodule WmDevForumWeb.PageController do
   use WmDevForumWeb, :controller
+  alias WmDevForum.UserManagement
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
+  end
+
+  def register(conn, _params) do
+    render(conn, "register.html")
+  end
+
+  def register_user(conn, params) do
+    _user = UserManagement.register_user(params)
+
+    conn =
+      conn
+      |> put_flash(:info, "You have been registered!!!!")
+
+    render(conn, "register.html")
   end
 end
